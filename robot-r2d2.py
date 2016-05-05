d384ba173f7d3e4f9305f8a88180dea945246cbc
perkovich <per@strm.se>
2016-05-06 00:42:27 +0200

    Notifier for charging, working and assumed MIA
    Some variables in swedish, will translate.

--- robot-r2d2.py
+++ robot-r2d2.py
@@ -3,22 +3,46 @@
 
 import RPi.GPIO as GPIO
 import time
-import urllib
+import urllib2
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-switch = 21
+from datetime import datetime, timedelta
 
+switch = 21
+laddar = 0
 GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
+now = datetime.now()
+sent_alert = 0
 
 while True:
   if GPIO.input(switch) == False:
    print("Laddar")
-   time.sleep(1)
+   if laddar == 1:
+     laddar = 0
+     response = urllib2.urlopen('http://10.0.176.46:8080/json.htm?type=command&param=switchlight&idx=2&switchcmd=Off')
+     html = response.read()
+   last_seen = datetime.now()
+   expected_back = datetime.now() + timedelta(seconds=10)
+
   else:
    print("Ute och klipper")
-   time.sleep(1)
+   if laddar == 0:
+     laddar = 1
+     response = urllib2.urlopen('http://10.0.176.46:8080/json.htm?type=command&param=switchlight&idx=2&switchcmd=On')
+     html = response.read()
+   laddar = 1
+   now = datetime.now()
+   if now >= expected_back:
+     if sent_alert == 0:
+       response = urllib2.urlopen('http://10.0.176.46:8080/json.htm?type=command&param=switchlight&idx=3&switchcmd=On')
+       html = response.read()
+       sent_alert = 1
+       print("Emergency!!")
+   else:
+    sent_alert = 0
+  time.sleep(1)
 
 GPIO.cleanup()
-
